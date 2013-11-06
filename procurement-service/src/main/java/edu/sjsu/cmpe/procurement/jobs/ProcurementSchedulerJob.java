6298081d0684f1ee9b16f395e756a4cd21fc4a14
Sina Nikkhah <sinasad@gmail.com>
2013-11-05 18:01:28 -0800

    fixed Jobs Folder

--- procurement-service/src/main/java/edu/sjsu/cmpe/procurement/jobs/ProcurementSchedulerJob.java
+++ procurement-service/src/main/java/edu/sjsu/cmpe/procurement/jobs/ProcurementSchedulerJob.java
@@ -39,7 +39,7 @@ import edu.sjsu.cmpe.procurement.ProcurementService;
 /**
  * This job will run at every 5 second.
  */
-@Every("5min")
+@Every("40s")
 public class ProcurementSchedulerJob extends Job {
     private final Logger log = LoggerFactory.getLogger(getClass());
     
